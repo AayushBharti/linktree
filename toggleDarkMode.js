6352efe9b74cf5066f87f1466e23804edd759153
Aayush Bharti <aayushbh2002@gmail.com>
2024-01-28 16:44:30 +0530

    minor bug fix

--- toggleDarkMode.js
+++ toggleDarkMode.js
@@ -1,28 +1,11 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    function changeIcon()
-    {   
-        // document.body.style.backgroundImage="url(./assets/Kingdom.jpeg)";
-        document.body.classList.toggle("dark-mode");
-    }
+function changeIcon()
+{   
+    // document.body.style.backgroundImage="url(./assets/Kingdom.jpeg)";
+    // document.body.classList.toggle("dark-mode");
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+    
+}
 
 
 
